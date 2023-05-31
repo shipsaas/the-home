14debd482be9f7822925ba4847b5ba6ce4b5deb8
Seth Phat <phattranminh96@gmail.com>
2023-05-31 09:16:10 +0700

    ShipSaaS

--- src/pages/ProjectPage/ProjectPage.vue
+++ src/pages/ProjectPage/ProjectPage.vue
@@ -1,20 +1,9 @@
 <template>
-  <div class="bg-white px-6 py-24 sm:py-32 lg:px-8">
-    <div class="mx-auto max-w-2xl text-center">
-      <p class="text-base font-semibold leading-7 text-indigo-600">
-        ShipSaaS Proudest Present
-      </p>
-      <h2
-        class="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl"
-      >
-        Our Projects - Our Works
-      </h2>
-      <p class="mt-6 text-lg leading-8 text-gray-600">
-        We empower every application by providing the awesome and reliable
-        helpers, libraries and even sustainable implemented system.
-      </p>
-    </div>
-  </div>
+  <ProjectBanner />
+  <ProjectList />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ProjectList from "@/pages/ProjectPage/components/ProjectList.vue";
+import ProjectBanner from "@/pages/ProjectPage/components/ProjectBanner.vue";
+</script>
